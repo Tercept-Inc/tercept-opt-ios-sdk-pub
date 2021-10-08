b0132d16a4a507e41afc4c4288c5b1d2f47957c3
Nitesh Kumawat <60547094+nkdanta1@users.noreply.github.com>
2021-10-08 09:59:55 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-import PackageDescription
+// swift-tools-version:4.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.import PackageDescription
 
 let package = Package(
     name: "TerceptSDK",
